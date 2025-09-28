fb504ec71eed36a7d7c8b84491148b3243c8dae7
nil000007 <babagolu477@gmail.com>
2025-09-28 20:09:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,29 +9,20 @@ load_dotenv()
 
 # VPS --- FILL COOKIES 🍪 in """ ... """ 
 
-INST_COOKIES = """
-# wtite up here insta cookies
-"""
 
-YTUB_COOKIES = """
-# write here yt cookies
-"""
-
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = os.getenv("27581835", "")
+API_HASH = os.getenv("184a11c9b4af04d78d4c1c181781498b", "")
+BOT_TOKEN = os.getenv(" 8074485587:AAFxiOkQb4NJpoc-3HTWy3lN-EGiHUXxzjU", "")
 MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
+OWNER_ID = list(map(int, os.getenv("6965618585", "").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
-STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
+
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1003139136725")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1003072916786")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
-YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
-INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+
+
+
+
 
